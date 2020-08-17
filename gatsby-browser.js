46e0ff32763fcffce2fd60cd98783f40445a53b5
Martin <mr.mmaurer+work@gmail.com>
2020-08-17 23:43:26 +0100

    Cleanup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { ContextProvider } from './src/context/context';
 
-// custom typefaces
-
-import "prismjs/themes/prism.css"
-
 export const wrapRootElement = ({ element }) => (
   <ContextProvider>
     {element}
