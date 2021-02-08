2c140a9a36504b41333e47fd7de684e2a008ecd0
Martin <mr.mmaurer+work@gmail.com>
2021-02-08 23:54:45 +0100

    Moved faq-prismic to separate folder

---
+++ src/pages/faq-prismic/index.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable camelcase */
+import React, { FC, Fragment } from 'react';
+import { graphql } from 'gatsby';
+import { RichText } from 'prismic-reactjs';
+
+const FaqPrismic: FC = ({ data }) => {
+  const { prismicFaqPage } = data;
+
+  const {
+    description, entries, title, title_group_1,
+  } = prismicFaqPage.data;
+
+  return (
+    <div className="wrapper">
+      <RichText
+        render={title.raw}
+      />
+      <RichText
+        render={description.raw}
+      />
+      <RichText
+        render={title_group_1.raw}
+      />
+      <dl>
+        {entries.map((entry: any) => (
+          <Fragment key={entry.question.text}>
+            <dt>
+              <RichText
+                render={entry.question.raw}
+              />
+            </dt>
+            <dd>
+              <RichText
+                render={entry.answer.raw}
+              />
+            </dd>
+          </Fragment>
+        ))}
+      </dl>
+    </div>
+  );
+};
+
+export default FaqPrismic;
+
+export const pageQuery = graphql`
+  query prismicFaqPage {
+    prismicFaqPage {
+      data {
+        title {
+          raw
+        }
+        description {
+          raw
+        }
+        title_group_1 {
+          raw
+        }
+        entries {
+          answer {
+            html
+            text
+            raw
+          }
+          question {
+            html
+            raw
+            text
+          }
+        }
+      }
+    }
+  }
+`;
